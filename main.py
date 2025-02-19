b36dfb44f6512e54e4fa7a558a71cd48be9cdbe4
George Kaceli <georgekaceli8@gmail.com>
2025-02-19 11:10:51 -0500

    changed main.py

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ python_ds = load_dataset("claudios/code_search_net", 'python')
 python_ds['train'].to_json("data/python_train.json")
 python_ds['test'].to_json("data/python_test.json")
 python_ds['validation'].to_json("data/python_val.json")
+
+java_ds = load_dataset("claudios/code_search_net", 'java')
