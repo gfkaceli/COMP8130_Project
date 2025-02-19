f25dd775157f132248bb5918b31ed95c754c7a84
George Kaceli <georgekaceli8@gmail.com>
2025-02-19 11:08:09 -0500

    data files

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from datasets import load_dataset
 
-import torch
-import os
+# where to run and load models
+
+python_ds = load_dataset("claudios/code_search_net", 'python')
+
+
+python_ds['train'].to_json("data/python_train.json")
+python_ds['test'].to_json("data/python_test.json")
+python_ds['validation'].to_json("data/python_val.json")
