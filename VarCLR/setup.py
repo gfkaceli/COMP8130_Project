f94a6adac4e9fbfa5f7cb3352984f56df4c5b05f
George Kaceli <georgekaceli8@gmail.com>
2025-03-16 16:23:12 -0400

    Add VarCLR as a regular folder instead of an embedded repository

---
+++ VarCLR/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="varclr",
+    version="1.0",
+    author="Qibin Chen",
+    author_email="qibinc@andrew.cmu.edu",
+    license="MIT",
+    python_requires=">=3.6",
+    packages=find_packages(exclude=[]),
+    install_requires=[
+        "black>=21.10b0",
+        "gdown>=4.2.0",
+        "isort>=5.8.0",
+        "pandas>=1.1.0",
+        "pre-commit>=2.15.0",
+        "pytest>=6.2.4",
+        "pytorch-lightning>=1.0.8,<1.3",
+        "sentencepiece>=0.1.95",
+        "scipy>=1.5.2",
+        "torch>=1.7.1",
+        "transformers>=4.5.1",
+        "wandb>=0.12.6",
+    ],
+)
---
+++ VarCLR/tests/reproduce/test_idbench.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+from varclr.benchmarks import Benchmark
+from varclr.models.model import Encoder
+
+
+def test_codebert():
+    model = Encoder.from_pretrained("varclr-codebert")
+
+    paper_results = {
+        ("small", "similarity"): 0.53,
+        ("medium", "similarity"): 0.53,
+        ("large", "similarity"): 0.51,
+        ("small", "relatedness"): 0.79,
+        ("medium", "relatedness"): 0.79,
+        ("large", "relatedness"): 0.80,
+    }
+    for (variant, metric), expected in paper_results.items():
+        b = Benchmark.build("idbench", variant=variant, metric=metric)
+        actual = b.evaluate(model.score(*b.get_inputs()))["spearmanr"]
+        assert np.allclose(actual, expected, atol=1e-2)
+
+
+def test_avg():
+    model = Encoder.from_pretrained("varclr-avg")
+
+    paper_results = {
+        ("small", "similarity"): 0.47,
+        ("medium", "similarity"): 0.45,
+        ("large", "similarity"): 0.44,
+        ("small", "relatedness"): 0.67,
+        ("medium", "relatedness"): 0.66,
+        ("large", "relatedness"): 0.66,
+    }
+    for (variant, metric), expected in paper_results.items():
+        b = Benchmark.build("idbench", variant=variant, metric=metric)
+        actual = b.evaluate(model.score(*b.get_inputs()))["spearmanr"]
+        assert np.allclose(actual, expected, atol=1e-2)
+
+
+def test_lstm():
+    model = Encoder.from_pretrained("varclr-lstm")
+
+    paper_results = {
+        ("small", "similarity"): 0.50,
+        ("medium", "similarity"): 0.49,
+        ("large", "similarity"): 0.49,
+        ("small", "relatedness"): 0.71,
+        ("medium", "relatedness"): 0.70,
+        ("large", "relatedness"): 0.69,
+    }
+    for (variant, metric), expected in paper_results.items():
+        b = Benchmark.build("idbench", variant=variant, metric=metric)
+        actual = b.evaluate(model.score(*b.get_inputs()))["spearmanr"]
+        assert np.allclose(actual, expected, atol=1e-2)
